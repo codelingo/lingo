1f342d5e5c08232d278de8a923287d0b064ea653
waigani <waigani@gmail.com>
2016-11-14 16:27:51 +1300

    review fix

--- scripts/replace_platform_vendor.sh
+++ scripts/replace_platform_vendor.sh
@@ -1,3 +1,3 @@
 rm -rf $GOPATH/src/github.com/codelingo/platform/vendor/github.com/codelingo/lingo
-cp -R $GOPATH/src/github.com/codelingo/lingo /home/blakemscurr/work/src/github.com/codelingo/platform/vendor/github.com/codelingo/lingo
+cp -R $GOPATH/src/github.com/codelingo/lingo $GOPATH/src/github.com/codelingo/platform/vendor/github.com/codelingo/lingo
 rm -rf $GOPATH/src/github.com/codelingo/platform/vendor/github.com/codelingo/lingo/vendor
