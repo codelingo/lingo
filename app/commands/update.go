f54e4caaddc78f7cf5c4ded6d048edc872f96587
Benjamin Rood <benjamin-rood@users.noreply.github.com>
2016-11-29 11:43:32 +1300

    Revert "Alias `lingo update` to `lingo setup --keep-creds`"

--- app/commands/update.go
+++ app/commands/update.go
@@ -1,82 +1,60 @@
 package commands
 
 import (
-	"flag"
+	"fmt"
 
 	"github.com/blang/semver"
+	"github.com/codelingo/lingo/app/util/common"
+
 	"github.com/codegangsta/cli"
 )
 
 func init() {
 	// TODO(waigani) support upgrades
 	// TODO(waigani) support specific versions
-
-	// NOTE: temporary solution – `lingo update` ⟼ `lingo setup --keed-creds`
 	register(&cli.Command{
-		Name:   "update",
-		Usage:  "update the lingo client to the latest release",
-		Flags:  []cli.Flag{},
+		Name:  "update",
+		Usage: "update the lingo client to the latest release",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "dry-run",
+				Usage: "prints the update steps without preforming them",
+			},
+			cli.BoolFlag{
+				Name:  "check",
+				Usage: "checks if a newer version is available",
+			},
+		},
 		Action: updateAction,
 	},
 		false,
 		homeRq,
-		authRq,
 	)
-
-	// 	register(&cli.Command{
-	// 	Name:  "update",
-	// 	Usage: "update the lingo client to the latest release",
-	// 	Flags: []cli.Flag{
-	// 		cli.BoolFlag{
-	// 			Name:  "dry-run",
-	// 			Usage: "prints the update steps without preforming them",
-	// 		},
-	// 		cli.BoolFlag{
-	// 			Name:  "check",
-	// 			Usage: "checks if a newer version is available",
-	// 		},
-	// 	},
-	// 	Action: updateAction,
-	// },
-	// 	false,
-	// 	homeRq,
-	// )
 }
 
 func updateAction(ctx *cli.Context) {
 
-	// NOTE: temporary solution – `lingo update` ⟼ `lingo setup --keep-creds`
-	fset := flag.NewFlagSet("update aliased to setup keep-creds", flag.ContinueOnError)
-	f := cli.BoolFlag{
-		Name:  "keep-creds",
-		Usage: "Preserves existing credentials (if present)",
+	fmt.Println("DISABLED: the update command will be enabled once the codelingo/lingo repository is public")
+
+	// first check if an update is avaliable
+	v, err := semver.Make(common.ClientVersion)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+		// errors.ErrorStack(err)
 	}
-	f.Apply(fset)
-	ctx = cli.NewContext(ctx.App, fset, ctx.Parent())
-	ctx.Set("keep-creds", "true")
-	setupLingoAction(ctx)
 
-	// fmt.Println("DISABLED: the update command will be enabled once the codelingo/lingo repository is public")
+	latest, err := latestVersion()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+		// errors.ErrorStack(err)
+	}
 
-	// // first check if an update is avaliable
-	// v, err := semver.Make(common.ClientVersion)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// 	// errors.ErrorStack(err)
-	// }
-
-	// latest, err := latestVersion()
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// 	// errors.ErrorStack(err)
-	// }
-
-	// if v.GT(latest) {
-	// 	// no new versions available
-	// 	return
-	// }
+	if v.GT(latest) {
+		// no new versions available
+		return
+	}
 
 	// CONTINUE HERE:
 	// 1. detect local OS
