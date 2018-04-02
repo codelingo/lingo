a81a25b97c75172345db0450a4a1333ac3be94f5
codelingo <hello@codelingo.io>
2018-04-03 11:56:40 +1200

    Update comment messages

--- new/lingo/cmd/flows.go
+++ new/lingo/cmd/flows.go
@@ -26,13 +26,8 @@ import (
 // flowsCmd represents the flows command
 var flowsCmd = &cobra.Command{
 	Use:   "flows",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List Flows",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := listFlows(cmd, args); err != nil {
 			fmt.Println(err.Error())
--- new/lingo/cmd/tenets.go
+++ new/lingo/cmd/tenets.go
@@ -28,13 +28,8 @@ import (
 // tenetsCmd represents the tenets command
 var tenetsCmd = &cobra.Command{
 	Use:   "tenets",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List Tenets",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := listTenets(cmd, args); err != nil {
 			fmt.Println(err.Error())
