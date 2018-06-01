f18ae0db7ecfd7d9c312fe13d9791b5a3e55bf5c
jzhu077 <zhujunyu168@hotmail.com>
2018-06-01 13:49:43 +1200

    fix error message of lexicons command

--- app/commands/lexicons.go
+++ app/commands/lexicons.go
@@ -191,7 +191,7 @@ func listFacts(ctx *cli.Context) error {
 		owner = args[0]
 		name = args[1]
 	} else {
-		return errors.New("Please specify a properly namespaced lexicon, ie,\nlingo lexicon list-facts codelingo/go")
+		return errors.New("Please specify a properly namespaced lexicon, ie,\nlingo lexicons list-facts codelingo/go")
 	}
 
 	facts, err := svc.ListFacts(owner, name, ctx.String("version"))
