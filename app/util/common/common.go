4305dc2f97ca48bb466e2820462a769efadf2766
Emerson Wood <emersonwood94@gmail.com>
2018-07-24 15:04:01 +1200

    Support .lingo.yaml and .lingo.yml file types (#366)

--- app/util/common/common.go
+++ app/util/common/common.go
@@ -1,3 +1,19 @@
 package common
 
+import (
+	"path/filepath"
+)
+
 const ClientVersion = "0.4.0"
+
+var LingoFilenames = map[string]bool{
+	".lingo":      true,
+	".lingo.yaml": true,
+	".lingo.yml":  true,
+}
+
+// IsDotlingoFile returns if that given filepath has a recognised lingo extension.
+func IsDotlingoFile(file string) bool {
+	filename := filepath.Base(file)
+	return LingoFilenames[filename]
+}
